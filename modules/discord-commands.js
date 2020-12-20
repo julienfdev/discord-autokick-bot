85f7486330ae9436b49189cfdc48f70ea4c7ab73
Julien FERAND <julien.ferand@gmail.com>
2020-12-20 12:09:04 +0100

    kick routine

--- modules/discord-commands.js
+++ modules/discord-commands.js
@@ -1,5 +1,7 @@
 const commandConfig = require('../config/config').commands;
+const discordConfig = require('../config/config').discordConfig;
 const fetch = require('node-fetch');
+const logger = require('./logger');
 
 const commandHandler = async (client) => {
     client.on('message', async (message) => {
@@ -7,14 +9,23 @@ const commandHandler = async (client) => {
         const rawCommand = message.content.slice(commandConfig.prefix.length);
         const commandArray = rawCommand.split(' ');
         const command = commandArray[0];
-        let args = undefined;
+        let args = [];
         if (commandArray.length > 1) {
-            args = commandArray.shift();
+            args = [...commandArray];
+            args.shift();
         }
-
         switch (command) {
             case 'haiku':
-                haiku(message);
+                haikuHandler(message);
+                break;
+            case 'kick':
+                // We first check if the author has the right to invoke the kick command
+                if (await messageAuthorAuthorized(message, commandConfig.kick.rolesAuthorized, client)) {
+                    // We use the kickHandler to react to the command
+                    kickHandler(client, message, args);
+                } else {
+                    message.reply(commandConfig.kick.messageUnauthorized);
+                }
                 break;
             default:
                 message.reply(commandConfig.defaultReply);
@@ -23,9 +34,9 @@ const commandHandler = async (client) => {
     })
 };
 
-const haiku = async (message) => {
-    const url = new URL(commandConfig.haikuUrl);
-    url.search = new URLSearchParams(commandConfig.haikuParameters).toString();
+const haikuHandler = async (message) => {
+    const url = new URL(commandConfig.haiku.haikuUrl);
+    url.search = new URLSearchParams(commandConfig.haiku.haikuParameters).toString();
 
     const responseObject = await fetch(url)
     let responseString = await responseObject.text();
@@ -33,6 +44,31 @@ const haiku = async (message) => {
     // Replying to the message
     const replyString = haikuParser(responseString);
     message.reply(`\n${replyString}`);
+}
+
+const kickHandler = async (client, message, args) => {
+    // First, we check that a single argument is provided
+    if (!args.length) return message.reply(commandConfig.kick.messageMissingArg);
+    if (args.length > 1) return message.reply(commandConfig.kick.messageTooManyArgs);
+
+    try {
+        // We try to get the member, if it throws an exception, it's likely due to an incorrect mention
+        const memberToKick = await getMember(client, tagToId(args[0]));
+        if(memberToKick){
+            // We then check if the member we want to kick is protected
+            if(!protectedRole(memberToKick.roles.cache.map(role => role.id), commandConfig.kick.rolesProtected)){
+                // If not protected, we can kick the user
+                kickRoutine(message, memberToKick);
+            }
+            else{
+                message.reply(commandConfig.kick.messageProtected);
+            }
+        }
+
+    } catch (error) {
+        message.reply(commandConfig.kick.messageNeedMention);
+        logger.error(error.message);
+    }
 }
 
 // need to be adapted to your Haiku generator
@@ -49,5 +85,59 @@ const haikuParser = (responseString) => {
     }
     return replyArray.join('\n');
 }
+
+// Funny kick routine
+const kickRoutine = (message, member) =>{
+    const channel = message.channel;
+
+    channel.send(formatKickString(commandConfig.kick.messageKickIntro, member));
+    setTimeout(() =>{
+        channel.send(formatKickString(commandConfig.kick.messageKickFirst, member));
+        channel.send(formatKickString(commandConfig.kick.messageKickSecond, member));
+    }, 3500);
+    setTimeout(()=>{
+        channel.send(formatKickString(commandConfig.kick.messageKickThird, member));
+    }, 7000);
+    setTimeout(()=>{
+        channel.send(formatKickString(commandConfig.kick.messageKickFourth, member));
+        channel.send(formatKickString(commandConfig.kick.messageKickFifth, member));
+    }, 10500)
+    setTimeout(()=>{
+        channel.send(formatKickString(commandConfig.kick.messageKickSixth, member));
+    }, 14000)
+    setTimeout(()=>{
+        channel.send(formatKickString(commandConfig.kick.messageKickSeventh, member));
+        member.kick();
+    }, 15000);
+}
+
+const messageAuthorAuthorized = async (message, authorizedRoles, client) => {
+    // First we get the roles the author has inside the guild
+    const guild = await client.guilds.fetch(discordConfig.guildId);
+    const member = await guild.members.fetch(message.author.id);
+    const roles = member.roles.cache.map(role => role.id);
+    // We try to match the roles of the author with the authorized roles
+    const matchingRoles = roles.filter(role => authorizedRoles.includes(role));
+    return Boolean(matchingRoles.length);
+};
+
+const tagToId = (tag) =>{
+    return tag.replace(/[\\<>@#&!]/g, "");
+}
+
+const getMember = async (client, id) => {
+    const guild = await client.guilds.fetch(discordConfig.guildId);
+    const member = await guild.members.fetch(id);
+
+    return member;
+}
+
+const protectedRole = (roles, protectedRoles) =>{
+    return Boolean(roles.filter(role => protectedRoles.includes(role)).length);
+};
+
+const formatKickString = (string, member) =>{
+    return string.replace('[member]', `<@${member.user.id}>`);
+};
 
 module.exports = commandHandler;
